c229f1a52619ced6b912a86223ebe4ffb306918c
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2024-11-18 19:39:00 -0300

    Atualizado

--- Algoritimos/Exercícios e  PDF's para resolve-los/Sistema de Gestão de Notas de Alunos - P2.py
+++ Algoritimos/Exercícios e  PDF's para resolve-los/Sistema de Gestão de Notas de Alunos - P2.py
@@ -3,7 +3,7 @@
 # Além disso, o sistema precisa salvar essas informações em um arquivo e ser capaz de lidar com 
 # possíveis erros de entrada e saída. Siga os requisitos abaixo para implementar a solução
 
-lista_de_alunos = []
+import os
 
 def adicionar_alunos(nome, notas, media, lista_de_alunos):
        
@@ -18,12 +18,39 @@ def adicionar_alunos(nome, notas, media, lista_de_alunos):
     print("aluno:",dicionario_alunos["nome"], "Adicionado com sucesso!")
 
 def salvar_em_arquivo(lista_de_alunos):
-    with open("Alunos.txt", "") as Arquivo:
-        Arquivo.write(lista_de_alunos)
+    
+    if os.path.exists("Alunos.txt"):
+        resposta = input("Arquivo Alunos.txt ja existe, deseja subscreve-lo: (sim/nao) ")
+        if resposta.lower() != "sim":
+            print("Digite corretamente")
+            return
+    
+    try:
+        with open("Alunos.txt", "w") as Arquivo:
+            for aluno in lista_de_alunos:
+                nome = aluno["nome"]
+                media = aluno["media"]
+                Arquivo.write(f"Nome: {nome}, Media: {media:.2f} \n")
+            print("Arquivo Alunos.txt Salvo com Sucesso!")
+            
+    except IOError as e:
+        print("Erro ao salvar o arquivo {e}")
+    
+def ordenar_alunos(lista_de_alunos):
+     for i in range(1, len(lista_de_alunos)):
+        key = lista_de_alunos[i]
+        j = i - 1
+        while j >= 0 and key["media"] > lista_de_alunos[j]["media"]:
+            lista_de_alunos[j + 1] = lista_de_alunos[j]
+            j -= 1
+        lista_de_alunos[j + 1] = key
 
 def main():
+    
+    lista_de_alunos = []
+    
     while True:
-        print("\nOpções: \n1. Adicionar aluno \n2. Salvar em Arquivo \n3. Sair")
+        print("\nOpções: \n1. Adicionar aluno \n2. Salvar em Arquivo \n3. Ordenar alunos por Media \n4. Sair")
         opcao = input("Escolha uma opção: ")
 
         if opcao == '1':
@@ -40,7 +67,15 @@ def main():
         elif opcao == '2':
             salvar_em_arquivo(lista_de_alunos)
         
+        elif opcao == '3':
+            ordenar_alunos(lista_de_alunos)
+            print("Os alunos foram ordenados por média!!")    
+        
+        elif opcao == '4':
+            print("Saindo....")
+            break
+        
         else:
-            break 
+            print("Opção inválida, Digite novamente") 
 main()
             
