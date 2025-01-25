af4ea8a392da3b9db5502cef3e2a928dbe92c12e
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2025-01-25 17:33:40 -0300

    add

--- Exercícios_Praticando/BLABALA.py
+++ Exercícios_Praticando/BLABALA.py
@@ -6,7 +6,19 @@ def cadastro(email, senha):
         "senha" : senha
     }
     
-   lista_de_usuarios.append(dic_login)
-   
-email = input("Email: ")
-senha = float(input("Senha: "))
+    lista_de_usuarios.append(dic_login)
+    print("Cadastrado")
+
+resultado = cadastro("lukajean745@gmail.com", "1907050603")
+print(resultado)
+
+def usuarios(lista_de_usuarios):
+    if not lista_de_usuarios:
+        print("Nenhum usuario Cadastrado!")
+    else:
+        for user in lista_de_usuarios:
+            print("Email:" ,user['email'], "Senha:" ,user['senha'],)
+            
+          
+    
+usuarios(lista_de_usuarios)
