e9036389bcf0e30f00289f9fd639652a5325e3a4
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2025-02-14 20:49:46 -0300

    add

---
+++ POO/Aula 1 P1.py
@@ -0,0 +1,13 @@
+#Defini a Classe
+class Pessoa:
+    #Definindo o metodo construtor e os atributos
+    def __init__(self, nome, sobrenome, idade, cpf):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+        self.cpf = cpf
+    
+    #Metodo
+    
+        
+        
