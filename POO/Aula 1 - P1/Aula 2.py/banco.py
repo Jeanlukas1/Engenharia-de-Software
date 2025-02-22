1c9ec2201b64d58296193da412fa86a231d68a48
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2025-02-21 21:40:06 -0300

    Add files via upload

---
+++ POO/Aula 1 - P1/Aula 2.py/banco.py
@@ -0,0 +1,70 @@
+#criar uma classe
+class Banco:
+    #método construtor
+    def __init__(self, banco, agencia, conta, cliente, cpf):
+        self.banco = banco
+        self.agencia = agencia
+        self.conta = conta
+        self.cliente = cliente
+        self.cpf = cpf
+        self.saldo = 0
+        self.senha = None
+
+    # Método de definição de Senha
+    def definir_senha(self, new_senha):
+        if self.senha == None:
+            self.senha = new_senha
+            print("Sua senha foi definida!")
+        elif self.senha != None:
+            decisao = input("Você já possui uma senha definida, Deseja mudar a senha mesmo assim?")
+            if decisao == "sim":
+                self.senha = new_senha
+        else:
+            print("Escolha errada!")
+
+    # Método de apresetação
+    def __str__(self):
+        return f"""
+Cliente {self.cliente} Cadastrado com sucesso!
+Conta: {self.conta}
+Agência: {self.agencia}
+Banco: {self.banco} 
+        """
+    # Deposito
+    def deposito(self, valor):
+        if isinstance(valor, (int, float)):
+            self.saldo += valor
+            print(f"Saldo atualizado com sucesso, no valor de R${float(valor)}.")
+    # Saque
+    def saque(self, senha, valor):
+        if self.senha == senha and self.saldo >= valor and isinstance(valor, (int, float)):
+            self.saldo -= valor
+            print(f"Saque realizado no valor de: {float(valor)}")
+        else:
+            print("Valor ou senha incorreta, Tente novamente!")
+    # Transferencia
+    # Pix
+    def pix(self, destinatario, valor):
+        if isinstance(valor, (int, float)) and self.saldo >= valor:
+            self.saldo -= valor
+            destinatario.saldo += valor
+            print(f"""
+Pix realizado no valor de R${valor}
+De: {self.cliente}
+Para: {destinatario.cliente}
+                  """)
+    # caixinha
+    # Extrato
+    def extrato(self):
+        print(
+            f"""
+--------------------------------------------
+Conta: {self.conta}
+Agência: {self.agencia}
+Saldo: {self.saldo}
+Cliente: {self.cliente}
+--------------------------------------------
+            """
+        )
+
+    
