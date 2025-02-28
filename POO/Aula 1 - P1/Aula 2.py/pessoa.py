62e0817d2164afa55512d78f9c9cf4cf0753e7bd
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2025-02-28 14:31:11 -0300

    add

--- POO/Aula 1 - P1/Aula 2.py/pessoa.py
+++
@@ -1,8 +0,0 @@
-class Pessoa:
-    def __init__(self, nome, genero, cpf):
-        self.nome = nome
-        self.genero = genero
-        self.cpf = cpf
-
-pessoa1 = Pessoa("Joao", "M", "123456")
-print(pessoa1.nome)
