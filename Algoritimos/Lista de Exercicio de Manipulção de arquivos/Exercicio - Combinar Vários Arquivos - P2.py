aaaf1eb51573f02a05a7e5809711f2cb807b3eb6
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2025-03-12 18:36:38 -0300

    Archive manipulation exercises

--- Algoritimos/Lista de Exercicio de Manipulção de arquivos/Exercicio - Combinar Vários Arquivos - P2.py
+++ Algoritimos/Lista de Exercicio de Manipulção de arquivos/Exercicio - Combinar Vários Arquivos - P2.py
@@ -2,15 +2,25 @@
 # `parte3.txt`). 
 #    - Combine o conteúdo dos três arquivos em um novo arquivo chamado `texto_completo.txt`
 
-with open("parte1.txt", "w") as arquivo1:
-    arquivo1.write("Ola Mundo!")
+def ler_arquivo(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
+            return arquivo.read()
+    except FileNotFoundError:
+        print(f"Erro: o arquivo {nome_arquivo} não foi encontrado.")
+        return ''
 
-with open("parte2.txt", "w") as arquivo2:
-    arquivo2.write("Sou muito")
-    
-with open("parte3.txt", "w") as arquivo3:
-    arquivo3.write("Bom!")    
+def combinar_arquivos():
+    parte1 = ler_arquivo('parte1.txt')
+    parte2 = ler_arquivo('parte2.txt')
+    parte3 = ler_arquivo('parte3.txt')
 
-with open("texto_completo", "w") as arquivo4:
-    arquivo4.write("")
-    
+    conteudo_completo = parte1 + '\n' + parte2 + '\n' + parte3
+
+    with open('texto_completo.txt', 'w', encoding='utf-8') as arquivo_completo:
+        arquivo_completo.write(conteudo_completo)
+
+    print("O conteúdo dos arquivos foi combinado em 'texto_completo.txt'.")
+
+combinar_arquivos()
+
